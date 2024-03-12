494236b8e675dfd136fa0a28debbe8bec45160f3
Neofoxxo <hamish@cits.co.nz>
2024-03-12 18:13:10 +1000

    Added robots.txt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-import basicSsl from "@vitejs/plugin-basic-ssl"
+// import basicSsl from "@vitejs/plugin-basic-ssl"
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		basicSsl({
-			name: "techconnectfrontend",
-		}),
+		// basicSsl({
+		// 	name: "techconnectfrontend",
+		// }),
 	],
 })
